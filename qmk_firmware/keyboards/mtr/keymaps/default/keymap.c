f79d2dd52e27ecfda5d28d68c927c83d19f679ce
mentako-ya <mentakoya@gmail.com>
2023-05-07 20:59:02 +0900

    マトリクス、キーマップ、LED追加

--- qmk_firmware/keyboards/mtr/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/mtr/keymaps/default/keymap.c
@@ -4,21 +4,27 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
-     */
-    [0] = LAYOUT_ortho_4x4(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+	     /*
+	      * ┌───┬───┐              ┌───┬───┬───┬───┐
+	      * │L1 │L2 │              │PGU│HOM│UP │END│
+	      * ├───┼───┤              ├───┼───┼───┼───┤
+	      * │L3 │BS │              │PGD│LT │DN │RT │
+	      * └───┴───┘              └───┴───┴───┴───┘
+          */
+    [0] = LAYOUT_6x2(
+        LT(1,KC_LNG2), LT(2,KC_SPC),   KC_PGUP,     KC_HOME,     KC_UP,       KC_END,
+		LT(3,KC_LNG1), KC_BSPC,        KC_PGDN,     KC_LEFT,     KC_DOWN,     KC_RGHT
+    ),
+    [1] = LAYOUT_6x2(
+        KC_NO,         KC_NO,          S(KC_PGUP),  S(KC_HOME),  S(KC_UP),    S(KC_END),
+		KC_NO,         KC_DEL,         S(KC_PGDN),  S(KC_LEFT),  S(KC_DOWN),  S(KC_RGHT)
+    ),
+    [2] = LAYOUT_6x2(
+        KC_NO,         KC_NO,          C(KC_PGUP),  C(KC_HOME),  C(KC_UP),    C(KC_END),
+		KC_NO,         KC_DEL,         C(KC_PGDN),  C(KC_LEFT),  C(KC_DOWN),  C(KC_RGHT)
+    ),
+    [3] = LAYOUT_6x2(
+        KC_NO,         KC_NO,          RGB_MOD,     RGB_HUI,     RGB_SAI,     RGB_VAI,
+		KC_NO,         RGB_TOG,        RGB_RMOD,    RGB_HUD,     RGB_SAD,     RGB_VAD
     )
 };
--- qmk_firmware/keyboards/mtr/rules.mk
+++ qmk_firmware/keyboards/mtr/rules.mk
@@ -1,1 +1,6 @@
 # This file intentionally left blank
+
+RGBLIGHT_ENABLE = yes        # Enable RGBLIGHT
+RGBLIGHT_DRIVER = WS2812
+RGB_MATRIX_ENABLE = no      # Enable RGB_MATRIX (not work yet)
+RGB_MATRIX_DRIVER = WS2812
